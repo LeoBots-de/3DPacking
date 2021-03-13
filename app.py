d0219eb732f1ae1bd7c14aa00430cdb270bf8c5c
Marco Zwillus <mz29zode@studserv.uni-leipzig.de>
2021-03-13 15:51:12 +0100

    implement api

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from packaging import Organizer, Article, Package, Space
 
 app = Flask(__name__)
+
+
+def solpacking(req):
+    packagelist = []
+    package_types = req['package_types']
+    for package_type in package_types:
+        print(package_type['dimensions'])
+        packagelist.append(Package(Space(package_type['dimensions']), package_type['cost']))
+        pass
+
+    articlelist = []
+    articles = req['articles']
+    for article in articles:
+        #TODO delete cost from Article
+        articlelist.append(Article(Space()))
+
+    organizer = Organizer(packagelist, articlelist)
+    organizer.solve()
+
+    return req
 
 
 @app.route('/')
@@ -8,5 +29,11 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/packaging', methods=['POST'])
+def packaging():
+    answer = solpacking(request.json)
+    return answer
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
